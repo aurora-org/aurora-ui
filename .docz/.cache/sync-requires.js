eb9e39e08e1a460b878c98ece0379bc5f10af18b
wanghuiyang <wang.huiyang@outlook.com>
2021-06-09 17:32:00 +0800

    feat: docz development enviroment
    BREAKING CHANGE: development enviroment

---
+++ .docz/.cache/sync-requires.js
@@ -0,0 +1,12 @@
+
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": (preferDefault(require("/home/wanghuiyang/aurora/aurora-ui/.docz/.cache/dev-404-page.js"))),
+  "component---components-alert-index-mdx": (preferDefault(require("/home/wanghuiyang/aurora/aurora-ui/Components/Alert/index.mdx"))),
+  "component---src-pages-404-js": (preferDefault(require("/home/wanghuiyang/aurora/aurora-ui/.docz/src/pages/404.js")))
+}
+
---
+++ doczrc.js
@@ -0,0 +1,6 @@
+export default {
+  files: './Components/**/*.{md,markdown,mdx}',
+  dest: 'doc-site',
+  title: 'aurora-ui',
+  typescript: true,
+}
